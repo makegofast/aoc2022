e113b6a42e85208b698d02835d4f43fe0065c7c3
Joe Webster <joe@makegofast.com>
2022-12-05 23:54:17 -0500

    Reorganizing skeleton slightly

--- app/dayN/__init__.py
+++ app/dayN/__init__.py
@@ -1,9 +1,7 @@
 class Solver(object):
     @staticmethod
     def get_data(filename):
-        with open(filename, 'rt') as fp:
-            while line := fp.readline().rstrip().split():
-                yield line[0], line[1]
+        return open(filename, 'rt').read():
 
     def solve_part_1(self, filename):
         pass
@@ -17,15 +15,16 @@ if __name__ == "__main__":
     if not solver.solve_part_1('test_data.txt') == 0:
         raise ValueError("Tests Failed")
 
+    print("Solving Part 1...")
+    solver = Solver()
+    if not solver.solve_part_1('data.txt') == 0:
+        raise ValueError("Test Failed")
+
     solver = Solver()
     print("Running Test Part 2...")
     if not solver.solve_part_2('test_data.txt') == 0:
         raise ValueError("Tests Failed")
 
-    print("Solving Part 1...")
-    solver = Solver()
-    if not solver.solve_part_1('data.txt') == 0:
-        raise ValueError("Test Failed")
 
     print("Solving Part 2...")
     solver = Solver()
